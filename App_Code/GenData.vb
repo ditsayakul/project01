680b0228abe46348dca7b2ed5818c595ffe4422b
ditsayakul <ditsayakul@yahoo.com>
2018-04-23 11:23:38 +0700

    project01 kpi

---
+++ App_Code/GenData.vb
@@ -0,0 +1,116 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports System.Web.Configuration
+Imports System.Xml.Serialization
+Imports System.Xml
+Imports Microsoft.VisualBasic
+Imports Oracle.DataAccess.Client
+Imports System.Data.Odbc
+
+Public Class GenData
+
+    Dim Ufile As UFile
+
+    Private _sql As String
+    Private _path As String
+    Private conn As New OracleConnection(System.Configuration.ConfigurationManager.AppSettings("Oraconn"))
+    Private connMysql As New OdbcConnection(System.Configuration.ConfigurationManager.AppSettings("Mysqlcon"))
+
+    Public Function gentext(ByVal path As String, ByVal sqltext As String) As Boolean
+
+        Dim strSQL As String = sqltext
+        Dim cmd As New OracleDataAdapter(strSQL, conn)
+        conn.Open()
+
+        Dim DT As DataTable = New DataTable()
+        Dim pathtext As String = path
+        Dim Ufile As New UFile
+        cmd.Fill(DT)
+
+        Dim _chk As Boolean = Ufile.Exporttext(pathtext, DT)
+        conn.Close()
+        Return _chk
+
+    End Function
+
+    Public Function gentextMysql(ByVal path As String, ByVal sqltext As String) As Boolean
+
+        Dim strSQL As String = sqltext
+        Dim dtReader As OdbcDataReader
+        Dim objCmd As OdbcCommand = New OdbcCommand(strSQL, connMysql)
+        connMysql.Open()
+        dtReader = objCmd.ExecuteReader()
+
+        Dim DT As DataTable = New DataTable()
+        Dim pathtext As String = path
+        Dim Ufile As New UFile
+        DT.Load(dtReader)
+
+        Dim _chk As Boolean = Ufile.Exporttext(pathtext, DT)
+        connMysql.Close()
+        Return _chk
+
+    End Function
+
+
+    Public Function genxml(ByVal path As String, ByVal sqltext As String) As Boolean
+
+        Dim strSQL As String = sqltext
+        Dim cmd As New OracleDataAdapter(strSQL, conn)
+        conn.Open()
+
+        Dim DT As DataTable = New DataTable()
+        Dim pathtext As String = path
+        Dim Ufile As New UFile
+        cmd.Fill(DT)
+
+        Dim _chk As Boolean = Ufile.WriteToXML(DT, pathtext)
+        conn.Close()
+        Return _chk
+
+    End Function
+
+    Public Function genvalue(ByVal sqltext As String) As String
+
+        Dim strSQL As String = sqltext
+        Dim cmd As New OracleDataAdapter(strSQL, conn)
+        Dim value As String
+
+        conn.Open()
+        'Dim myReaderit As SqlClient.SqlDataReader = cmd.ExecuteReader()
+        'If myReaderit.Read() = True Then
+        '    value = myReaderit.GetValue(0)
+        Return value
+        'End If
+
+        conn.Close()
+    End Function
+
+    Public Property sql() As String
+        Get
+            Return _sql
+        End Get
+        Set(ByVal value As String)
+            _sql = value
+        End Set
+    End Property
+
+    Public Property path() As String
+        Get
+            Return _path
+        End Get
+        Set(ByVal value As String)
+            _path = value
+        End Set
+    End Property
+End Class
---
+++ App_Code/UFile.vb
@@ -0,0 +1,123 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.IO
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Xml.Serialization
+Imports System.Xml
+Imports System.Text
+Imports System.Web.UI.HtmlControls
+Imports System.Web
+
+Public Class UFile
+
+    Public Function WriteToXML(ByVal dt As DataTable, ByVal fileName As String) As Boolean
+        'write the datatable to xml 
+        Dim _chk As Integer
+        Try
+            Dim xml As String = "<?xml version=""1.0"" encoding=""UTF-8""?>"
+            xml += "<pie></pie>"
+            Dim doc As New XmlDocument()
+            doc.LoadXml(xml)
+            Dim xw As New XmlTextWriter(fileName, System.Text.Encoding.UTF8)
+            Dim rootElement As XmlElement = doc.DocumentElement
+            For Each row As DataRow In dt.Rows
+
+                For i As Integer = 0 To dt.Columns.Count - 1
+                    Dim colElement As XmlElement() = New XmlElement(dt.Columns.Count - 1) {}
+                    'กำหนด Element 
+                    colElement(i) = doc.CreateElement("slice")
+                    'กำหนด Attribute
+                    colElement(i).SetAttribute("title", Mid(row(0).ToString(), 2, row(0).ToString().Length()))
+
+                    colElement(i).SetAttribute("color", row(2).ToString())
+
+                    If i = 1 Then
+
+                        colElement(i).InnerText = Fix(row(i).ToString())
+                        rootElement.AppendChild(colElement(i))
+
+                    End If
+
+                Next
+
+            Next
+            doc.WriteTo(xw)
+            xw.Close()
+
+            _chk = 1
+        Catch ex As Exception
+            _chk = 0
+        End Try
+
+        If _chk = 1 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+
+    Public Function Exporttext(ByVal path As String, ByVal table As DataTable) As Boolean
+        Dim output As New StreamWriter(path, False, UnicodeEncoding.Default)
+        Dim _chk As Integer
+        Try
+            For Each row As DataRow In table.Rows
+                For Each value As Object In row.ItemArray
+                    output.Write(value)
+                    'output.WriteLine()
+                Next
+                output.WriteLine()
+            Next
+            output.Close()
+
+            _chk = 1
+        Catch ex As Exception
+            _chk = 0
+        End Try
+
+        If _chk = 1 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    'Public Function Readtext(ByVal path As String) As String
+    '    Dim StrWer As StreamReader
+
+    '    Dim _error As Boolean
+    '    Dim txt_er As String
+
+    '    Try
+    '        StrWer = File.OpenText(path)
+    '        Do Until StrWer.EndOfStream
+    '            'test = StrWer.ReadLine() & "<br>"
+    '        Loop
+    '        StrWer.Close()
+    '        _error = True
+    '        'test = "Files Writed."
+    '    Catch ex As Exception
+    '        _error = False
+    '        'test = "Read failed. (" & ex.Message & ")"
+    '    End Try
+    '    If _error = True Then
+
+    '        If IsNothing(path) Then
+    '            txt_er = ""
+    '        End If
+
+    '        Return txt_er
+    '    Else
+    '        txt_er = "ไม่สามารถอ่านข้อมูลได้"
+    '        Return txt_er
+    '    End If
+
+    'End Function
+
+End Class
+
+
+
